faa79877a8c65903cf79cb3a4367251aaceb6b3b
j-0-n-e-z <j000neeez@gmail.com>
2023-04-24 16:13:16 +0500

    add mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,46 +3,50 @@ import {SharePopup} from './SharePopup'
 export default function App() {
 	return (
 		<main className='w-full min-h-screen grid place-items-center bg-lightGrayishBlue'>
-			<article className='flex w-[730px] h-[280px] bg-white shadow-xl rounded-lg'>
-				<div className='w-[285px] h-full rounded-s-lg overflow-hidden'>
+			<article className='lg:mt-0 -mt-2 flex lg:flex-row flex-col lg:w-[730px] w-[87%] lg:h-[280px] bg-white lg:shadow-xl shadow-md rounded-lg'>
+				<div className='lg:w-[285px] lg:h-full h-[200px] lg:rounded-s-lg rounded-t-lg overflow-hidden'>
 					<img
-						className='w-full h-full object-cover object-left'
+						className='w-full h-full object-cover lg:object-left object-[0_-15px]'
 						src='./assets/images/drawers.jpg'
 						alt='drawers'
 					/>
 				</div>
-				<div className='font-[Manrope] py-[33px] px-10 flex-1 relative'>
-					<h2 className='text-xl tracking-[0.015rem] text-veryDarkGrayishBlue font-bold'>
+				<div className='font-[Manrope] lg:py-[33px] pt-9 pb-7 lg:px-10 px-8 flex-1 relative rounded-lg lg:overflow-visible overflow-hidden'>
+					<h2 className='lg:text-xl tracking-[0.01rem] text-veryDarkGrayishBlue font-bold'>
 						Shift the overall look and feel by adding these wonderful touches to
 						furniture in your home
 					</h2>
-					<p className='text-desaturatedDarkBlue text-[13px] mt-[11px] tracking-lightlyWide leading-[1.25rem]'>
+					<p className='text-desaturatedDarkBlue text-[13px] lg:mt-[11px] mt-[12px] tracking-lightlyWide leading-[1.25rem] mb-7'>
 						Ever been in a room and felt like something was missing? Perhaps it
 						felt slightly bare and uninviting. I’ve got some simple tips to help
 						you make any room feel complete.
 					</p>
-					<div className='flex items-center mt-5'>
-						<div className='rounded-full w-10 h-10 overflow-hidden'>
-							<img src='./assets/images/avatar-michelle.jpg' alt='michelle' />
-						</div>
-						<div className='flex flex-col ml-4 pt-[1px]'>
-							<h3 className='text-veryDarkGrayishBlue text-[13px] font-bold tracking-lightlyWide'>
-								Michelle Appleton
-							</h3>
-							<span className='text-grayishBlue text-[13px] tracking-lightlyWide'>
-								28 Jun 2020
-							</span>
-						</div>
-						<div className='group relative mb-[1px] ml-auto w-8 h-8 bg-lightGrayishBlue rounded-full grid place-items-center cursor-pointer hover:bg-desaturatedDarkBlue transition'>
-							<svg
-								className='fill-[#6E8098] group-hover:fill-white transition'
-								xmlns='http://www.w3.org/2000/svg'
-								width='15'
-								height='13'
-							>
-								<path d='M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z' />
-							</svg>
+					<div className='flex flex-row-reverse items-center lg:relative lg:mt-5 mt-3 h-10'>
+						<div className='group peer flex w-8 h-8 ml-auto items-center pt-3'>
+							<div className='z-10 peer mb-[1px] w-8 h-8 bg-lightGrayishBlue rounded-full grid place-items-center cursor-pointer group-hover:bg-desaturatedDarkBlue transition'>
+								<svg
+									className='fill-[#6E8098]  group-hover:fill-white transition'
+									xmlns='http://www.w3.org/2000/svg'
+									width='15'
+									height='13'
+								>
+									<path d='M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z' />
+								</svg>
+							</div>
 							<SharePopup />
+						</div>
+						<div className='lg:peer-hover:flex peer-hover:hidden flex'>
+							<div className='rounded-full w-10 h-10 overflow-hidden '>
+								<img src='./assets/images/avatar-michelle.jpg' alt='michelle' />
+							</div>
+							<div className='flex flex-col ml-4 pt-[1px]'>
+								<h3 className='text-veryDarkGrayishBlue text-[13px] font-bold tracking-lightlyWide'>
+									Michelle Appleton
+								</h3>
+								<span className='text-grayishBlue text-[12px] tracking-wider lg:mt-0 mt-[2px]'>
+									28 Jun 2020
+								</span>
+							</div>
 						</div>
 					</div>
 				</div>
--- src/SharePopup.tsx
+++ src/SharePopup.tsx
@@ -7,8 +7,8 @@ import {
 
 export const SharePopup = () => {
 	return (
-		<div className='cursor-auto hidden group-hover:block absolute bottom-[20px] h-24'>
-			<div className='flex px-[38px] items-center relative w-[250px] h-[55px] bg-veryDarkGrayishBlue rounded-xl text-lightGrayishBlue'>
+		<div className='cursor-auto hidden hover:block peer-hover:block absolute lg:bottom-[30px] lg:w-[250px] w-full bottom-0 left-0 lg:left-auto lg:-right-[108px] lg:h-[88px]'>
+			<div className='flex lg:px-[38px] px-8 items-center relative lg:w-[250px] w-full lg:h-[55px] h-20 bg-veryDarkGrayishBlue lg:rounded-xl text-lightGrayishBlue'>
 				<span className='text-grayishBlue tracking-[.3rem] uppercase text-[13px] pt-1'>
 					share
 				</span>
@@ -23,7 +23,7 @@ export const SharePopup = () => {
 					</div>
 					<a href='#'>
 						<FontAwesomeIcon
-							className='hover:text-white transition text-[20px]'
+							className='hover:text-white transition text-[20px] mt-1'
 							icon={faTwitter}
 						/>
 					</a>
@@ -34,7 +34,7 @@ export const SharePopup = () => {
 						/>
 					</a>
 				</div>
-				<div className='absolute -bottom-[12px] left-[50%] -translate-x-[50%] w-0 h-0 border-t-veryDarkGrayishBlue border-[12px] border-b-0 border-l-transparent border-r-transparent '></div>
+				<div className='lg:block hidden absolute -bottom-[12px] left-[50%] -translate-x-[50%] w-0 h-0 border-t-veryDarkGrayishBlue border-[12px] border-b-0 border-l-transparent border-r-transparent '></div>
 			</div>
 		</div>
 	)
